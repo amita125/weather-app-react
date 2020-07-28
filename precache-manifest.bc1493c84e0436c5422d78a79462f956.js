1e92ba1d68d2e0d9201015f25825e0a68cd594a0
amita125 <amitaghale@gmail.com>
2020-07-28 01:38:03 +0100

    Updates

---
+++ precache-manifest.bc1493c84e0436c5422d78a79462f956.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f663291b798782ba4513271aee79fd",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "55e5ba0b004aa1f6e378",
+    "url": "/weather-app-react/static/css/main.e956ef84.chunk.css"
+  },
+  {
+    "revision": "c25d7b42e24361e20969",
+    "url": "/weather-app-react/static/js/2.0b443818.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/weather-app-react/static/js/2.0b443818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e5ba0b004aa1f6e378",
+    "url": "/weather-app-react/static/js/main.f037f0a6.chunk.js"
+  },
+  {
+    "revision": "0965184a592c71c34fe2",
+    "url": "/weather-app-react/static/js/runtime-main.0d456eba.js"
+  },
+  {
+    "revision": "6a3816528f4faf7542d27994678be0aa",
+    "url": "/weather-app-react/static/media/clear.6a381652.jpg"
+  },
+  {
+    "revision": "ef790a4b785f3ef1e86b614e0c35a97e",
+    "url": "/weather-app-react/static/media/clear.ef790a4b.svg"
+  },
+  {
+    "revision": "b40129ad82ebbda043dbc91d8f10e027",
+    "url": "/weather-app-react/static/media/clouds.b40129ad.svg"
+  },
+  {
+    "revision": "5fe7bfdb31644db145335c59962b3b39",
+    "url": "/weather-app-react/static/media/cloudy.5fe7bfdb.jpg"
+  },
+  {
+    "revision": "91b1ceb9d4d1d7ea8816725b0ac86956",
+    "url": "/weather-app-react/static/media/drizzle.91b1ceb9.svg"
+  },
+  {
+    "revision": "119b1728afd6e50c9e8cfde83a59ee5d",
+    "url": "/weather-app-react/static/media/lightning.119b1728.jpg"
+  },
+  {
+    "revision": "6671130e11fea0854a766912408852a5",
+    "url": "/weather-app-react/static/media/mist.6671130e.jpg"
+  },
+  {
+    "revision": "4ce8407e85eeb03f7247e231556a10a7",
+    "url": "/weather-app-react/static/media/rain.4ce8407e.svg"
+  },
+  {
+    "revision": "ade49d8d4ca18f2fd7dc64dc0c9e72ae",
+    "url": "/weather-app-react/static/media/rain.ade49d8d.jpg"
+  },
+  {
+    "revision": "f59b02762cdab1c3e6beb1acb53b7ebc",
+    "url": "/weather-app-react/static/media/search.f59b0276.svg"
+  },
+  {
+    "revision": "5831cc74f8184c3a6b972fba489ad459",
+    "url": "/weather-app-react/static/media/snow.5831cc74.svg"
+  },
+  {
+    "revision": "beb6b32ce5f316e775e01cface020b38",
+    "url": "/weather-app-react/static/media/snow.beb6b32c.jpg"
+  },
+  {
+    "revision": "4b969a4c6cc99b09168f1458b5cf3499",
+    "url": "/weather-app-react/static/media/storm.4b969a4c.svg"
+  },
+  {
+    "revision": "d086d94347f7b88436adeb9cdbd3a617",
+    "url": "/weather-app-react/static/media/storm.d086d943.jpg"
+  },
+  {
+    "revision": "ebec5dbf733da491630dd0487c74658b",
+    "url": "/weather-app-react/static/media/thunderstorm.ebec5dbf.svg"
+  },
+  {
+    "revision": "57540e3203c3c42dad5d4e998b1b81b9",
+    "url": "/weather-app-react/static/media/tornado.57540e32.jpg"
+  },
+  {
+    "revision": "b3cf14c3c450f3feeddee689ed01e76f",
+    "url": "/weather-app-react/static/media/tornado.b3cf14c3.svg"
+  }
+]);
