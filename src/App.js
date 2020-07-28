815a3fb8cd4a8cb36c762f4689385744959a7502
amita125 <amitaghale@gmail.com>
2020-07-28 02:15:15 +0100

    app file updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch, HashRouter as Router } from "react-router-dom";
 
 import MainPage from "./Components/MainPage";
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <Router>
           <Switch>
-            <Route path="/" exact component={MainPage} />
+            <Route
+              path='/'
+              exact
+              component={MainPage}
+            />
           </Switch>
-        </HashRouter>
+        </Router>
       </div>
     );
   }
